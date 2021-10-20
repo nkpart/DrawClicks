85367006ef29bcc2440f4c7dffce978cea2488c3
Nick Partridge <nkpart@gmail.com>
2021-10-20 16:07:29 +1000

    Initial Commit

---
+++ Shared/DrawClicksApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawClicksApp.swift
+//  Shared
+//
+//  Created by Nick Partridge on 20/10/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawClicksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
